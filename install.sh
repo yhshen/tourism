aeb5b65b0c815bd93837edd7b8d64a5f39462568
yhshen <yhshen2012@gmail.com>
2016-05-12 04:43:39 +0800

    add scrapy 1.0 source code and compile and install command

--- install.sh
+++ install.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
+set -x
 
 # refer to 
 # http://scrapy-chs.readthedocs.io/zh_CN/latest/intro/install.html#intro-install-platform-notes
 
+path=`pwd`
+lib="$path/lib"
+scrapy_decompress_cmd="unzip"
+scrapy_archive_name="scrapy-1.0.zip"
+scrapy_file_name="scrapy-1.0"
+scrapy_install_cmd="sudo python"
 
+#install scrapy
+
+cd $lib 
+if [ ! -f $lib/$scrapy_archive_name ]; then
+    echo "scrapy archive not exist"
+    exit -1;
+fi
+
+if [ -d $lib/$scrapy_file_name ]; then
+    rm -fR $lib/$scrapy_file_name
+fi
+
+`$scrapy_decompress_cmd $lib/$scrapy_archive_name -d $lib`
+
+cd $lib/$scrapy_file_name
+if [ ! -f "$lib/$scrapy_file_name/setup.py"]; then
+    echo "not exist setup file for scrapy"
+    exit -1
+else
+    cd $lib/$scrapy_file_name 
+    $scrapy_install_cmd setup.py build 
+    $scrapy_install_cmd setup.py install 
+fi
+
+exit 0
